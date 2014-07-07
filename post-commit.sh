2e41feb1868a9765da8c843a081da4346622a895
SvenFlorian <florianminges@gmail.com>
2014-07-07 21:48:17 +0200

    Updated git hook with correct permissions too

--- post-commit.sh
+++ post-commit.sh
@@ -3,31 +3,32 @@
 branch=$(git symbolic-ref --short HEAD)
 
 if [ "source" == "$branch" ]; then
-    # Do something
-    jekyll build
+echo "in the loop"
+# Do something
+jekyll build
 # should add check here if everything is committed
-    git checkout -b tmp
+git checkout -b tmp
 
 # delete everything except _site
 rm -rf $(ls * | grep -v _site)
 rm -f .gitignore
-git checkout master -- .gitignore
+git checkout gh-pages -- .gitignore
 git add .
 git add -u .
 git commit -m "Updated site"
-git checkout master
+git checkout gh-pages
 rm -r *
 git checkout tmp -- _site
 for file in _site/*
 do
-    cp -r $file ./
+cp -r $file ./
 done
 rm -r _site
 git add .
 git add -u .
 git commit -m "Updated site"
 git branch -D tmp
-git push origin master
+git push origin gh-pages
 git checkout source
 
 fi

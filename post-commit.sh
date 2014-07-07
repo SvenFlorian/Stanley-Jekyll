2d25767fb89d1888cde17edc4fcabaa4ef522092
SvenFlorian <florianminges@gmail.com>
2014-07-07 21:05:04 +0200

    Wrote git-hook

--- post-commit.sh
+++ post-commit.sh
@@ -2,7 +2,32 @@
 
 branch=$(git symbolic-ref --short HEAD)
 
-if [ "master" == "$branch" ]; then
+if [ "source" == "$branch" ]; then
     # Do something
-    # Use 3rd temporary branch
+    jekyll build
+# should add check here if everything is committed
+    git checkout -b tmp
+
+# delete everything except _site
+rm -rf $(ls * | grep -v _site)
+rm -f .gitignore
+git checkout master -- .gitignore
+git add .
+git add -u .
+git commit -m "Updated site"
+git checkout master
+rm -r *
+git checkout tmp -- _site
+for file in _site/*
+do
+    cp -r $file ./
+done
+rm -r _site
+git add .
+git add -u .
+git commit -m "Updated site"
+git branch -D tmp
+git push origin master
+git checkout source
+
 fi

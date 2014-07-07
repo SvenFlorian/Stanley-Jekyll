755bbd2c8b0afa8bac22824192791a048a161403
SvenFlorian <florianminges@gmail.com>
2014-07-07 21:51:13 +0200

    Update git hook

--- post-commit.sh
+++ post-commit.sh
@@ -5,30 +5,33 @@ branch=$(git symbolic-ref --short HEAD)
 if [ "source" == "$branch" ]; then
 echo "in the loop"
 # Do something
-jekyll build
+jekyll build;
+echo "Built site with Jekyll"
 # should add check here if everything is committed
-git checkout -b tmp
+git checkout -b tmp;
 
 # delete everything except _site
-rm -rf $(ls * | grep -v _site)
-rm -f .gitignore
-git checkout gh-pages -- .gitignore
-git add .
-git add -u .
-git commit -m "Updated site"
-git checkout gh-pages
-rm -r *
-git checkout tmp -- _site
+rm -rf $(ls * | grep -v _site);
+rm -f .gitignore;
+git checkout gh-pages -- .gitignore;
+git add .;
+git add -u .;
+git commit -m "Updated site";
+git checkout gh-pages;
+rm -r *;
+git checkout tmp -- _site;
 for file in _site/*
 do
-cp -r $file ./
+cp -r $file ./ ;
 done
-rm -r _site
-git add .
-git add -u .
-git commit -m "Updated site"
-git branch -D tmp
-git push origin gh-pages
-git checkout source
+rm -r _site;
+git add .;
+git add -u .;
+git commit -m "Updated site";
+git branch -D tmp;
+git push origin gh-pages;
+git checkout source;
+
+echo "Page updated"
 
 fi
